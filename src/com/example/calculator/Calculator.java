b07867ece5df8b3833dd4598b4a9f2b473eb306e
kdongyeon <91926630+kdongyeon@users.noreply.github.com>
2025-03-05 17:21:11 +0900

    lv1 최종

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -60,7 +60,8 @@ public class Calculator {
                         break;
                     default:
                         System.out.println("연산자 기호를 잘못입력했습니다.");
-                        continue;
+
+
                 }
                 break;
 
