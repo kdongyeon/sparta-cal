6a858d8a3fcae19569e04f2016c81258c4e79559
kdongyeon <91926630+kdongyeon@users.noreply.github.com>
2025-03-06 16:15:19 +0900

    lv2 수정중

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -9,38 +9,36 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         /* 반복문 시작 */
-        boolean exit = true;
-        int num1;
-        int num2;
 
-        while (exit) {    // exit 값이 true 일때만 반복
-            while (true) {
-                System.out.println("첫 번째 값을 입력 :");
-                num1 = sc.nextInt();         // 숫자(정수) 입력받기!
-                if (num1 < 0) {
-                    System.out.println("양의 정수만 입력하세요");
-                    continue;
-                }
-                System.out.println("두 번째 값을 입력 :");
-                num2 = sc.nextInt();
-                if (num2 < 0) {
-                    System.out.println("양의 정수만 입력하세요");
-                    continue;
-                }
 
-                System.out.println("+,-,*,/ 중에 하나만 입력하세요.");
-                char operation = sc.next().charAt(0);
+        // exit 값이 true 일때만 반복
+        String exit = "";
+        while (exit.equals("exit")) {
+            System.out.println("첫 번째 값을 입력 :");
+            int num1 = sc.nextInt();         // 숫자(정수) 입력받기!
+            if (num1 < 0) {
+                System.out.println("양의 정수만 입력하세요");
+                continue;
+            }
+            System.out.println("두 번째 값을 입력 :");
+            int num2 = sc.nextInt();
+            if (num2 < 0) {
+                System.out.println("양의 정수만 입력하세요");
+                continue;
+            }
+            //사칙연산 기호 입력받기
+            System.out.println("+,-,*,/ 중에 하나만 입력하세요.");
+            char operation = sc.next().charAt(0);
 
-                // 연산 후 결과 //
-                int result = Calculator.startCal(num1, num2, operation);
+            // 연산 후 결과 //
+            int result = Calculator.startCal(num1, num2, operation);
 
-                String end = sc.nextLine();
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String end = sc.nextLine();
 
-                if (end.equals("exit")) {
-                    exit = false;
-                    System.out.println("계산기 종료");   // 종료
-                }
+            if (end.equals("exit")) {
+                System.out.println("계산기 종료");
+                // 종료
             }
         }
     }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,12 +1,15 @@
 package com.example.calculator;
 
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Calculator {
+    private final List<Integer> resultList = new ArrayList<>();
 
     // 계산 메서드
-    static int startCal(int num1, int num2, char operation){
+    public static int startCal(int num1, int num2, char operation) {
 
-        int result=0;
+        int result;
         switch (operation) {
             case '+':
                 result = num1 + num2;
@@ -29,10 +32,21 @@ public class Calculator {
                 break;
             default:
                 System.out.println("연산자 기호를 잘못입력했습니다.");
-
+                return result;
         }
-            return result;
-
+    }
+        //Setter
+        public void setResultList (Integer result){
+            this.resultList.add(result);
         }
 
-}
+        //Getter
+        public List<Integer> getResultlist () {
+            return this.resultList;
+        }
+
+        //가장 먼저 저장된 데이터를 삭제하는 기능
+        public void removeResult () {
+            this.resultList.remove(0);
+        }
+    }
